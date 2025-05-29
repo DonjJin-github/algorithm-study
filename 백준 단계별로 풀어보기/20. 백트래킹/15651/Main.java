db062f06a589041bf98e356e951a0626819ce50a
dongjin <cdj7235@naver.com>
2025-05-29 11:38:30 +0900

    solve 15651 15652

---
+++ 백준 단계별로 풀어보기/20. 백트래킹/15651/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] arr ;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr= new int [N+1]; 
+        func(0,N,M);
+        bw.flush();
+        
+    }
+    static void func(int K, int N, int M)throws IOException{
+        if(K==M){
+            for(int i=0;i<M;i++){
+                bw.write(arr[i]+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i=1;i<=N;i++){
+            arr[K] = i;
+            func(K+1, N, M);
+        }
+    }
+}
