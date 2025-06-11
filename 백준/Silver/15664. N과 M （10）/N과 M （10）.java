971ec70aca44026f57b60cb6825f3ff0e602d366
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2025-06-11 10:15:54 +0900

    [Silver II] Title: N과 M (10), Time: 116 ms, Memory: 15788 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] arr;
+    static int[] printans;
+    
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        arr = new int [N];
+        printans = new int [M];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        func(0,0, N, M);
+        bw.flush();
+        
+    }
+    static void func (int index, int count , int N, int M)throws IOException{
+        if(count==M){
+            for(int i=0;i<M;i++){
+                bw.write(printans[i]+" ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        int lastnum = -1;
+        for(int i=index;i<N;i++){
+            if(lastnum!=arr[i]){
+            lastnum = arr[i];
+            printans[count] = arr[i];
+            func(i+1,count+1, N, M);
+            }
+        }
+    }
+}
