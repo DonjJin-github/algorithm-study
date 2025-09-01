516a0facbe7c5c55556bf7e00d9f6b34c6607e09
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2025-09-01 12:13:10 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        Integer[] B = new Integer[N];
+        int ans = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            ans +=A[i]*B[i];
+        }
+
+        bw.write(ans + "\n");
+        bw.flush();
+    }
+}
