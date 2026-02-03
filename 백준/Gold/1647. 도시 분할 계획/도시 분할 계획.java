af64f04707e89f31017b7dd1456bf48a5a2d66e7
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-02-03 12:48:02 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1544 ms, Memory: 352316 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+    static boolean check[];
+
+    static class Node implements Comparable<Node> {
+        int u;
+        int v;
+        int cost;
+
+        public Node(int u, int v, int cost) {
+            this.u = u;
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node newNode) {
+            return Integer.compare(cost, newNode.cost);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        check = new boolean[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(new int[] { v, cost });
+            graph.get(v).add(new int[] { u, cost });
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        int cv = 0;
+        long ans = 0;
+        int max = Integer.MIN_VALUE;
+
+        check[1] = true;
+        for (int[] temp : graph.get(1)) {
+            pq.add(new Node(1, temp[0], temp[1]));
+        }
+
+        while (cv < N - 1) {
+            Node cur = pq.poll();
+
+            if (check[cur.v])
+                continue;
+
+            ans += cur.cost;
+            max = Math.max(cur.cost,max);
+            cv++;
+            check[cur.v] = true;
+
+            for (int[] temp : graph.get(cur.v)) {
+                if (check[temp[0]])
+                    continue;
+
+                pq.add(new Node(cur.v, temp[0], temp[1]));
+            }
+        }
+
+        bw.write(ans-max+"\n");
+        bw.flush();
+
+    }
+}
