8b27759a8c796842869ff33432914a224934d4ac
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-02-05 22:59:06 +0900

    [level 3] Title: 숫자 게임, Time: 120.34 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int j=0;
+        for(int i=0;i<A.length;i++){
+            if(j==A.length)
+                break;
+            
+            if(A[j] < B[i]){
+                answer++;
+                j++;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
