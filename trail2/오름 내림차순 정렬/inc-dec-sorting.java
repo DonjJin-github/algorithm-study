758920a1f4cca796da128a2afb1dd527eaa3938f
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-06-23 22:07:18 +0900

    [Passed] 오름 내림차순 정렬 (145ms, 10MB)

---
+++ trail2/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            sb.append(arr[i]+" ");
+        }
+        System.out.println(sb.toString());
+        sb = new StringBuilder();
+        for(int i=n-1;i>=0;i--){
+            sb.append(arr[i]+" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
