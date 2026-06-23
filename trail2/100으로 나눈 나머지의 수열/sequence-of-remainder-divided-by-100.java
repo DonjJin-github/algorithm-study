532fe770babffeee3701baf756c135b7e5ce20df
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-06-23 21:21:59 +0900

    [Passed] 100으로 나눈 나머지의 수열 (138ms, 10MB)

---
+++ trail2/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.print(cal(n));
+    }
+    static int cal(int n){
+        if(n == 1)
+            return 2;
+        if(n == 2)
+            return 4;
+
+        return (cal(n-1) * cal(n-2))%100;
+    }
+}
