491931b922cb533f2324717837b8d1a19e4c5fcc
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-18 17:18:05 +0900

    [Passed] 수들의 합 최대화하기 (210ms, 12MB)

---
+++ trail4/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static boolean[] visit;
+    static int[][] arr;
+    static int ans = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        visit = new boolean[N];
+        arr = new int[N][N];
+
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        choose(0, new ArrayList<>());
+        bw.write(ans+"\n");
+        bw.flush();
+    }
+    static void choose(int x, ArrayList<Integer> list){
+        if(list.size() == N){
+            int sum = 0;
+            for(int temp : list)
+                sum+=temp;
+            ans = Math.max(ans, sum);
+            return;
+        }
+
+        for(int i=x;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(visit[j])
+                    continue;
+                visit[j] = true;
+                list.add(arr[i][j]);
+                choose(i+1, list);
+                visit[j] = false;
+                list.remove(list.size()-1);
+            }
+        }
+    }
+
+}
