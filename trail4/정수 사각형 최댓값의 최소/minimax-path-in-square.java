dee2d75846c0b6da3be9e3b069f40f81ad72f0ce
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-20 19:24:36 +0900

    [Passed] 정수 사각형 최댓값의 최소 (166ms, 12MB)

---
+++ trail4/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[][] arr;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        dp = new int[N][N];
+
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = arr[0][0];
+
+        for(int i=1;i<N;i++){
+            dp[0][i] = Math.max(arr[0][i], dp[0][i-1]);
+            dp[i][0] = Math.max(arr[i][0], dp[i-1][0]);
+        }
+
+        for(int i=1;i<N;i++){
+            for(int j=1;j<N;j++){
+                dp[i][j] = Math.min(Math.max(dp[i-1][j], arr[i][j]), Math.max(dp[i][j-1], arr[i][j]));
+            }
+        }
+
+        bw.write(dp[N-1][N-1]+"\n");
+        bw.flush();
+        
+    }
+}
