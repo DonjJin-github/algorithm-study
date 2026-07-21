51d8bb6648b281e025f194d1ebad78a902844974
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-21 23:35:58 +0900

    [Passed] 특별한 문자 (393ms, 24MB)

--- trail5/특별한 문자/special-character.java
+++ trail5/특별한 문자/special-character.java
@@ -8,27 +8,17 @@ public class Main {
 
         String[] str = br.readLine().split("");
         String ans = "None";
-        HashMap<String, int[]> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
         for(int i=0;i<str.length;i++){
             String key = str[i];
-            int[] value = map.getOrDefault(key,new int[]{i,0});
-            map.put(key,new int[]{i, value[1]+1});
+            int value = map.getOrDefault(key,0);
+            map.put(key,value+1);
         }
 
-        ArrayList<int[]> list = new ArrayList<>();
-        for(String key : map.keySet()){
-            int cur[] = map.get(key);
-            list.add(new int[]{cur[0], cur[1]});
-        }
-
-        Collections.sort(list,(a,b)->{
-            return Integer.compare(a[0],b[0]);
-        });
-        for(int i=0;i<list.size();i++){
-            int cur[] = list.get(i);
-            if(cur[1]==1){
-                ans = str[cur[0]];
+        for(int i=0;i<str.length;i++){
+            if(map.get(str[i])==1){
+                ans = str[i];
                 break;
             }
         }
