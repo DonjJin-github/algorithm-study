52b8e65e3c672d5ba882edb93047e074d0809448
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-23 19:16:02 +0900

    [Passed] 가장 오래 걸리는 학생 2 (532ms, 26MB)

---
+++ trail5/가장 오래 걸리는 학생 2/longest-student-2.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] distance;
+    static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+    static class  Node implements Comparable<Node>{
+        int v;
+        int cost;
+
+        Node(int v, int cost){
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node newNode){
+            return Integer.compare(cost, newNode.cost);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        distance = new int[N+1];
+        for(int i=0;i<=N;i++){
+            graph.add(new ArrayList<>());
+            distance[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph.get(v).add(new int[]{u,cost});
+        }
+
+        djk(N);
+        int ans = 0;
+        for(int i=1;i<N;i++){
+            ans = Math.max(ans, distance[i]);
+        }
+
+        bw.write(ans+"\n");
+        bw.flush();
+    }
+    static void djk(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        
+        pq.add(new Node(start, 0));
+        distance[start] = 0;
+        
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+
+            if(cur.cost != distance[cur.v])
+                continue;
+            
+            for(int[] next : graph.get(cur.v)){
+                int d = next[1] + cur.cost;
+                if(d<distance[next[0]]){
+                    distance[next[0]] = d;
+                    pq.add(new Node(next[0],d));
+                }
+            }
+        }
+    }
+}
