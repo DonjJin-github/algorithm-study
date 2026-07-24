895b0e63a702955f041afe86d27a78894afd4b21
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-24 21:26:20 +0900

    [Passed] 정수 N개의 합 3 (295ms, 21MB)

---
+++ trail5/정수 N개의 합 3/sum-of-n-integers-3.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] sum = new int[N+1][N+1];
+
+        sum[0][0] = 0;
+
+        for(int i=1;i<=N;i++){
+            sum[i][0] = 0;
+            sum[0][i] = 0;
+        }
+
+        for(int i=1;i<=N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=N;j++){
+                int x = Integer.parseInt(st.nextToken());
+                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + x;
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for(int i=K;i<=N;i++){
+            for(int j=K;j<=N;j++){
+                int a1 = sum[i][j];
+                int a2 = sum[i][j-K]+sum[i-K][j];
+                ans = Math.max(ans, a1-a2+sum[i-K][j-K]);
+            }
+        }
+
+        bw.write(ans+"\n");
+        bw.flush();
+    }
+}
