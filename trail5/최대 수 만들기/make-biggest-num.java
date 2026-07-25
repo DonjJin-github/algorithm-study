4fd328df183fb71fe7e2aefa140e772b33643686
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-25 22:09:24 +0900

    [Passed] 최대 수 만들기 (1051ms, 81MB)

--- trail5/최대 수 만들기/make-biggest-num.java
+++ trail5/최대 수 만들기/make-biggest-num.java
@@ -7,27 +7,23 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
         Integer[] arr = new Integer[N];
+        
         for(int i=0;i<N;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr, (a,b)->{
+        Arrays.sort(arr,(a,b)->{
             StringBuilder sb = new StringBuilder();
             String x = sb.append(a).append(b).toString();
-
             sb = new StringBuilder();
             String y = sb.append(b).append(a).toString();
 
             return y.compareTo(x);
         });
-        
-        for(int i=0;i<N;i++){
-            bw.write(arr[i]+"");
-        }
 
+        for(int print : arr)
+            bw.write(print+"");
         bw.flush();
-        
     }
 }
