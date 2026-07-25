cacc1f0280c1f8e1fa5e4278124b158a88427784
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-25 22:46:38 +0900

    [Passed] G & H 반전시키기 (107ms, 9MB)

--- trail5/G & H 반전시키기/reversing-g-and-h.java
+++ trail5/G & H 반전시키기/reversing-g-and-h.java
@@ -1,67 +1,27 @@
-// import java.util.*;
-// import java.io.*;
-
-// public class Main {
-//     public static void main(String[] args) throws IOException {
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-//         int N = Integer.parseInt(br.readLine());
-//         String[] a = br.readLine().split("");
-//         String[] b = br.readLine().split("");        
-
-//         int ans = 0;
-//         int idx = 0;
-//         while(idx<N){
-//             if(a[idx].equals(b[idx])){
-//                 idx++;
-//                 continue;
-//             }
-//             for(int j=idx;j<N;j++){
-//                 if(a[j].equals(b[j]) && !a[idx].equals(a[j])){
-//                     idx = j;
-//                     break;
-//                 }
-//             }
-//             ans++; 
-//         }
-//         bw.write(ans+"\n");
-//         bw.flush();
-//     }
-// }
-import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         String[] a = br.readLine().split("");
         String[] b = br.readLine().split("");
 
         int ans = 0;
-        int count = 0;
         int idx = 0;
-        while(idx<N-1){
-            if(a[idx].equals(b[idx])){
+
+        while (idx < N) {
+            if (a[idx].equals(b[idx])) {
                 idx++;
                 continue;
             }
-            for(int j=idx;j<N;j++){
-                if(a[j].equals(b[j]) && a[idx]!=a[j]){
-                    idx = j;
-                    break;
-                }
-                if(j==N-1){
-                    idx = j;
-                }
+            while (idx < N && !a[idx].equals(b[idx])) {
+                idx++;
             }
             ans++;
         }
-        bw.write(ans+"\n");
-        bw.flush();
-        
+
+        System.out.println(ans);
     }
 }
