d39496f7cda546d4e9ca5d1e11698d08be2a0cc9
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-26 01:04:36 +0900

    [Passed] K개의 벽 없애기 (97ms, 9MB)

--- trail4/K개의 벽 없애기/remove-k-walls.java
+++ trail4/K개의 벽 없애기/remove-k-walls.java
@@ -2,8 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int arr[][];
-    static int distance[][][];
     static int dx[] = {-1, 0, 1, 0};
     static int dy[] = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
@@ -13,13 +11,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        arr = new int[N][N];
-        distance = new int[N][N][K+1];
+        int[][] arr = new int[N][N];
+        int[][][] distance = new int[N][N][K+1];
 
         for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                Arrays.fill(distance[i][j],-1);
-            }
+            for(int j=0;j<N;j++)
+                Arrays.fill(distance[i][j], -1);
         }
 
         for(int i=0;i<N;i++){
@@ -28,15 +25,15 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int[] start = new int[2];
-        int[] end = new int[2];
+        int start[] = new int[2];
+        int end[] = new int[2];
         st = new StringTokenizer(br.readLine());
         start[0] = Integer.parseInt(st.nextToken())-1;
         start[1] = Integer.parseInt(st.nextToken())-1;
         st = new StringTokenizer(br.readLine());
         end[0] = Integer.parseInt(st.nextToken())-1;
         end[1] = Integer.parseInt(st.nextToken())-1;
-
+        
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{start[0], start[1], 0});
         distance[start[0]][start[1]][0] = 0;
@@ -50,14 +47,14 @@ public class Main {
 
                 if(nx<0 || nx>=N || ny<0 || ny>=N)
                     continue;
-                
-                if(arr[nx][ny]==1){
-                    if(cur[2]<K && distance[nx][ny][cur[2]+1] == -1){
-                        q.add(new int[]{nx, ny, cur[2]+1});
+                if(arr[nx][ny] == 1){
+                    if(cur[2]<K){
+                        if(distance[nx][ny][cur[2]+1] != -1)
+                            continue;
                         distance[nx][ny][cur[2]+1] = distance[cur[0]][cur[1]][cur[2]]+1;
+                        q.add(new int[]{nx, ny, cur[2]+1});
                     }
-                }
-                else{
+                }else{
                     if(distance[nx][ny][cur[2]] != -1)
                         continue;
                     distance[nx][ny][cur[2]] = distance[cur[0]][cur[1]][cur[2]]+1;
@@ -68,10 +65,9 @@ public class Main {
 
         int ans = Integer.MAX_VALUE;
         for(int i=0;i<=K;i++){
-            if(distance[end[0]][end[1]][i]==-1)
+            if(distance[end[0]][end[1]][i] == -1)
                 continue;
-            else
-                ans = Math.min(ans, distance[end[0]][end[1]][i]);
+            ans = Math.min(ans, distance[end[0]][end[1]][i]);
         }
         if(ans == Integer.MAX_VALUE)
             bw.write("-1\n");
