e8a82d707073df950e63358fdca2dc238bd4e6ed
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-07-26 00:40:09 +0900

    [Passed] 연결된 정점 (441ms, 33MB)

--- trail6/연결된 정점/connected-vertex.java
+++ trail6/연결된 정점/connected-vertex.java
@@ -12,11 +12,10 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         p = new int[N+1][2];
-        for(int i=0;i<N;i++){
+        for(int i=0;i<=N;i++){
             p[i][0] = -1;
             p[i][1] = 1;
         }
-
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
             String op = st.nextToken();
@@ -24,11 +23,9 @@ public class Main {
                 int u = Integer.parseInt(st.nextToken());
                 int v = Integer.parseInt(st.nextToken());
                 uni(u,v);
-            }
-            else{
+            }else{
                 int u = Integer.parseInt(st.nextToken());
                 int root = find(u);
-                    
                 bw.write(p[root][1]+"\n");
             }
         }
@@ -37,28 +34,28 @@ public class Main {
     static int find(int x){
         if(p[x][0]<0)
             return x;
-        int idx = find(p[x][0]);
-        p[x][0] = idx;
-        p[x][1] = p[idx][1];
-        return idx;
+        int root = find(p[x][0]);
+        p[x][0] = root;
+        p[x][1] = p[root][1];
+        return root;
     }
 
     static boolean uni(int u, int v){
         u = find(u);
         v = find(v);
-        
+
         if(u==v)
             return false;
+        
         if(p[v][0]<p[u][0]){
             int temp = u;
             u = v;
             v = temp;
         }
-        if(p[u][0] == p[v][0])
+        if(p[v][0] == p[u][0])
             p[u][0]--;
         p[v][0] = u;
-        p[v][1] = p[v][1] + p[u][1];
-        p[u][1] = p[v][1];
+        p[u][1] = p[v][1] + p[u][1];
         return true;
     }
 }
