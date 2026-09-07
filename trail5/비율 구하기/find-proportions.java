6fbfe4021f91a2a4f863c20ac2f38384c7395757
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-09-07 21:10:30 +0900

    [Passed] 비율 구하기 (337ms, 15MB)

---
+++ trail5/비율 구하기/find-proportions.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        for(int i=0;i<N;i++){
+            String key = br.readLine();
+            int count = map.getOrDefault(key, 0);
+
+            map.put(key, count+1);
+        }     
+
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+
+        while(it.hasNext()){
+            Map.Entry<String, Integer> entry = it.next();
+            bw.write(entry.getKey()+" ");
+            double ans = entry.getValue()/(double)N*100;
+            bw.write(String.format("%.4f\n", ans));
+        }   
+        bw.flush();
+    }
+}
