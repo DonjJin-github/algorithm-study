a49ebeaf21ec0b61a7b5dadd6e07ae6460901be7
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-09-09 00:14:56 +0900

    [Passed] 친한 점 (452ms, 31MB)

---
+++ trail5/친한 점/frendly-point.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static class Node implements Comparable<Node>{
+        int x;
+        int y;
+
+        Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Node newNode){
+            if(x == newNode.x){
+                return Integer.compare(y, newNode.y);
+            }
+            return Integer.compare(x, newNode.x);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        TreeSet<Node> set = new TreeSet<>();
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            set.add(new Node(num1, num2));
+        }
+
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            Node print = set.ceiling(new Node(num1, num2));
+            if(print == null)
+                bw.write("-1 -1\n");
+            else
+                bw.write(print.x+" "+print.y+"\n");
+        }
+
+        bw.flush();
+    }
+}
