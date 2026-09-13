a9b2aa4757c86107e5846fc655cd4fe04aa9dbc1
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2026-09-14 00:27:48 +0900

    [Passed] 배열 추출 (413ms, 30MB)

---
+++ trail5/배열 추출/array-extraction.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<N;i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num==0){
+                if(pq.isEmpty()){
+                    bw.write("0\n");
+                }else{
+                    bw.write(pq.poll()+"\n");
+                }
+            }else{
+                pq.add(num);
+            }
+        }
+        bw.flush();
+    }
+}
